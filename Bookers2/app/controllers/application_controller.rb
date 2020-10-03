056140bfd0c3515c246d80b44025f4dd84ca8280
EC2 Default User <ec2-user@ip-172-31-74-209.ec2.internal>
2020-10-03 17:51:56 +0000

    (update Bookers2)10/4

--- Bookers2/app/controllers/application_controller.rb
+++ Bookers2/app/controllers/application_controller.rb
@@ -1,13 +1,30 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+    
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :email, :username, :password, :password_confirmation ]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
+  
 end
+  # protect_from_forgery with: :exception
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # protected
+
+  # def configure_permitted_parameters
+  #   added_attrs = [ :email, :name, :password, :password_confirmation ]
+  #   devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  #   devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  #   devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  # end
+  
+  # def after_sign_in_path_for(resource)
+  #   show_user_path(:id)
+  # end
